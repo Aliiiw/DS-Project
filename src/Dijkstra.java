1cc3718a7685def57450f1bbbb172918de6deb25
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-13 09:41:33 +0430

    create classes Node and HeapNode

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,2 +1,20 @@
 public class Dijkstra {
+
+    static class HeapNode{
+        int vertex;
+        int distance;
+    }
+
+    static class Node{
+        int source;
+        int destination;
+        int weight;
+
+        public Node(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+
+    }
 }
